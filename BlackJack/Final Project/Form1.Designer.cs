3f630e04d65656b322405ef3d3a28ce3746b3138
Hector Maceo <84miner4@gmail.com>
2022-11-18 11:31:02 -0800

    Add Files

---
+++ BlackJack/Final Project/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DhiddenCard = new System.Windows.Forms.PictureBox();
+            this.Card2 = new System.Windows.Forms.PictureBox();
+            this.Card1 = new System.Windows.Forms.PictureBox();
+            this.DshowCard = new System.Windows.Forms.PictureBox();
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.HitBtn = new System.Windows.Forms.Button();
+            this.StandBtn = new System.Windows.Forms.Button();
+            this.Dcard2 = new System.Windows.Forms.PictureBox();
+            this.labelPlayerScore = new System.Windows.Forms.Label();
+            this.labelDealerScore = new System.Windows.Forms.Label();
+            this.labelStack = new System.Windows.Forms.Label();
+            this.numericUpDownBet = new System.Windows.Forms.NumericUpDown();
+            this.labelBet = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DhiddenCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DshowCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dcard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DhiddenCard
+            // 
+            this.DhiddenCard.Location = new System.Drawing.Point(158, 22);
+            this.DhiddenCard.Name = "DhiddenCard";
+            this.DhiddenCard.Size = new System.Drawing.Size(72, 106);
+            this.DhiddenCard.TabIndex = 1;
+            this.DhiddenCard.TabStop = false;
+            this.DhiddenCard.Click += new System.EventHandler(this.DhiddenCard_Click);
+            // 
+            // Card2
+            // 
+            this.Card2.Location = new System.Drawing.Point(158, 320);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(72, 106);
+            this.Card2.TabIndex = 3;
+            this.Card2.TabStop = false;
+            this.Card2.Click += new System.EventHandler(this.Card2_Click);
+            // 
+            // Card1
+            // 
+            this.Card1.Location = new System.Drawing.Point(66, 320);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(72, 106);
+            this.Card1.TabIndex = 2;
+            this.Card1.TabStop = false;
+            this.Card1.Click += new System.EventHandler(this.Card1_Click);
+            // 
+            // DshowCard
+            // 
+            this.DshowCard.Location = new System.Drawing.Point(66, 22);
+            this.DshowCard.Name = "DshowCard";
+            this.DshowCard.Size = new System.Drawing.Size(72, 106);
+            this.DshowCard.TabIndex = 4;
+            this.DshowCard.TabStop = false;
+            this.DshowCard.Click += new System.EventHandler(this.DshowCard_Click);
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Location = new System.Drawing.Point(570, 305);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(131, 24);
+            this.PlayBtn.TabIndex = 5;
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // HitBtn
+            // 
+            this.HitBtn.Location = new System.Drawing.Point(570, 335);
+            this.HitBtn.Name = "HitBtn";
+            this.HitBtn.Size = new System.Drawing.Size(131, 24);
+            this.HitBtn.TabIndex = 6;
+            this.HitBtn.Text = "Hit Me";
+            this.HitBtn.UseVisualStyleBackColor = true;
+            this.HitBtn.Click += new System.EventHandler(this.HitBtn_Click);
+            // 
+            // StandBtn
+            // 
+            this.StandBtn.Location = new System.Drawing.Point(570, 365);
+            this.StandBtn.Name = "StandBtn";
+            this.StandBtn.Size = new System.Drawing.Size(131, 24);
+            this.StandBtn.TabIndex = 7;
+            this.StandBtn.Text = "Stand";
+            this.StandBtn.UseVisualStyleBackColor = true;
+            this.StandBtn.Click += new System.EventHandler(this.StandBtn_Click);
+            // 
+            // Dcard2
+            // 
+            this.Dcard2.Location = new System.Drawing.Point(158, 22);
+            this.Dcard2.Name = "Dcard2";
+            this.Dcard2.Size = new System.Drawing.Size(72, 106);
+            this.Dcard2.TabIndex = 8;
+            this.Dcard2.TabStop = false;
+            this.Dcard2.Visible = false;
+            this.Dcard2.Click += new System.EventHandler(this.Dcard2_Click);
+            // 
+            // labelPlayerScore
+            // 
+            this.labelPlayerScore.AutoSize = true;
+            this.labelPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelPlayerScore.Location = new System.Drawing.Point(256, 351);
+            this.labelPlayerScore.Name = "labelPlayerScore";
+            this.labelPlayerScore.Size = new System.Drawing.Size(36, 39);
+            this.labelPlayerScore.TabIndex = 9;
+            this.labelPlayerScore.Text = "0";
+            // 
+            // labelDealerScore
+            // 
+            this.labelDealerScore.AutoSize = true;
+            this.labelDealerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelDealerScore.Location = new System.Drawing.Point(256, 52);
+            this.labelDealerScore.Name = "labelDealerScore";
+            this.labelDealerScore.Size = new System.Drawing.Size(36, 39);
+            this.labelDealerScore.TabIndex = 10;
+            this.labelDealerScore.Text = "0";
+            // 
+            // labelStack
+            // 
+            this.labelStack.AutoSize = true;
+            this.labelStack.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelStack.Location = new System.Drawing.Point(551, 402);
+            this.labelStack.Name = "labelStack";
+            this.labelStack.Size = new System.Drawing.Size(196, 39);
+            this.labelStack.TabIndex = 11;
+            this.labelStack.Text = "Stack: $100";
+            // 
+            // numericUpDownBet
+            // 
+            this.numericUpDownBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownBet.Location = new System.Drawing.Point(728, 330);
+            this.numericUpDownBet.Name = "numericUpDownBet";
+            this.numericUpDownBet.Size = new System.Drawing.Size(60, 30);
+            this.numericUpDownBet.TabIndex = 12;
+            this.numericUpDownBet.ValueChanged += new System.EventHandler(this.numericUpDownBet_ValueChanged);
+            // 
+            // labelBet
+            // 
+            this.labelBet.AutoSize = true;
+            this.labelBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelBet.Location = new System.Drawing.Point(735, 293);
+            this.labelBet.Name = "labelBet";
+            this.labelBet.Size = new System.Drawing.Size(41, 25);
+            this.labelBet.TabIndex = 13;
+            this.labelBet.Text = "Bet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelBet);
+            this.Controls.Add(this.numericUpDownBet);
+            this.Controls.Add(this.labelStack);
+            this.Controls.Add(this.labelDealerScore);
+            this.Controls.Add(this.labelPlayerScore);
+            this.Controls.Add(this.Dcard2);
+            this.Controls.Add(this.StandBtn);
+            this.Controls.Add(this.HitBtn);
+            this.Controls.Add(this.PlayBtn);
+            this.Controls.Add(this.DshowCard);
+            this.Controls.Add(this.Card2);
+            this.Controls.Add(this.Card1);
+            this.Controls.Add(this.DhiddenCard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DhiddenCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DshowCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dcard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox DhiddenCard;
+        private System.Windows.Forms.PictureBox Card2;
+        private System.Windows.Forms.PictureBox Card1;
+        private System.Windows.Forms.PictureBox DshowCard;
+        private System.Windows.Forms.Button PlayBtn;
+        private System.Windows.Forms.Button HitBtn;
+        private System.Windows.Forms.Button StandBtn;
+        private System.Windows.Forms.PictureBox Dcard2;
+        private System.Windows.Forms.Label labelPlayerScore;
+        private System.Windows.Forms.Label labelDealerScore;
+        private System.Windows.Forms.Label labelStack;
+        private System.Windows.Forms.NumericUpDown numericUpDownBet;
+        private System.Windows.Forms.Label labelBet;
+    }
+}
+
---
+++ BlackJack/Final Project/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class Form1 : Form
+    {
+        
+        string[] cardPath = Directory.GetFiles(@"..\..\.\Cards\Cards\", "*.png");
+        int[] cardValues = {10, 10, 10, 10,
+            2, 2, 2, 2,
+            3, 3, 3, 3,
+            4, 4, 4, 4,
+            5, 5, 5, 5,
+            6, 6, 6, 6,
+            7, 7, 7, 7,
+            8, 8, 8, 8,
+            9, 9, 9, 9,
+            0, 0, 0, 0, // 36-39
+            10, 10, 10, 10,
+            10, 10, 10, 10,
+            10, 10, 10, 10};
+        List<Int32> cardPlaced = new List<Int32>();
+        int card1, card2, card3, card4, stack = 100, playerScore = 0, dealerScore = 0;
+        int posX = 66, posY = 206, hit = 0;
+        bool play = false;
+
+        const string dir = @"..\..\.\Cards\Cards\";
+        Random random = new Random();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HitBtn.Enabled = false;
+            StandBtn.Enabled = false;
+            PlayBtn.Enabled = true;
+            numericUpDownBet.Enabled = true;
+
+            DhiddenCard.Image = Image.FromFile(dir + "red_back.png");
+            DhiddenCard.SizeMode = PictureBoxSizeMode.Zoom;
+            DshowCard.Image = Image.FromFile(dir + "red_back.png");
+            DshowCard.SizeMode = PictureBoxSizeMode.Zoom;
+
+            Card1.Image = Image.FromFile(dir + "red_back.png");
+            Card1.SizeMode = PictureBoxSizeMode.Zoom;
+            Card2.Image = Image.FromFile(dir + "red_back.png");
+            Card2.SizeMode = PictureBoxSizeMode.Zoom;
+
+
+            List<string> images = cardPath.ToList();
+
+        }
+
+        private void Dcard2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Card1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Card2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DhiddenCard_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void DshowCard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDownBet_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownBet.Maximum = stack;
+        }
+
+        private void StandBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            play = true;
+            PlayBtn.Enabled = false;
+            HitBtn.Enabled = true;
+            StandBtn.Enabled = true;
+            numericUpDownBet.Enabled = false;
+
+            card1 = random.Next(0, 51); card2 = random.Next(0, 51);
+            card3 = random.Next(0, 51); card4 = random.Next(0, 51);
+
+
+            while ((card1 == card2) || (card1 == card3) || (card1 == card4)
+                  || (card2 == card3) || (card2 == card4) || (card4 == card3))
+            {
+                if ((card1 == card2) || (card1 == card3) || (card1 == card4))
+                {
+                    card1 = random.Next(0, 51);
+                    cardPlaced.Add(card1);
+                }
+                else if ((card2 == card3) || (card2 == card4))
+                {
+                    card2 = random.Next(0, 51);
+                    cardPlaced.Add(card2);
+                }
+                else if (card4 == card3)
+                {
+                    card3 = random.Next(0, 51);
+                    cardPlaced.Add(card3);
+                }
+            }
+
+            DshowCard.ImageLocation = cardPath[card1];
+            DshowCard.SizeMode = PictureBoxSizeMode.Zoom;
+            Dcard2.ImageLocation = cardPath[card2];
+            Dcard2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            Card1.ImageLocation = cardPath[card3];
+            Card1.SizeMode = PictureBoxSizeMode.Zoom;
+            Card2.ImageLocation = cardPath[card4];
+            Card2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            playerScore += cardValues[card3];
+            playerScore += cardValues[card4];
+
+            labelPlayerScore.Text = playerScore.ToString();
+
+
+
+        }
+
+        private void getPlayerScore() {
+
+            if (cardValues[card3] == 0)
+            {
+                int calc = playerScore;
+                if(playerScore + 11 > 21)
+                {
+
+                }
+
+            }
+
+
+            playerScore += cardValues[card3];
+            playerScore += cardValues[card4];
+
+            labelPlayerScore.Text = playerScore.ToString();
+
+        }
+
+
+
+        private void HitBtn_Click(object sender, EventArgs e)
+        {
+            int nextCard = random.Next(0, 51);
+
+            PictureBox newCard = new PictureBox();
+            Point placement = new Point(posX, posY);
+
+            /*
+            do
+            {
+                for(int i = 0; i < cardPlaced.Count; i++)
+                {
+                    if(nextCard == cardPlaced[i])
+                    {
+
+                    }
+                }
+            } while (true);
+            */
+
+            if (play == true)
+            {
+                newCard.ImageLocation = cardPath[nextCard];
+                newCard.SizeMode = PictureBoxSizeMode.Zoom;
+                newCard.Width = Card1.Width; // width of initial card on form
+                newCard.Height = Card1.Height; // height of initial card on form
+                newCard.Location = placement;
+                this.Controls.Add(newCard);
+                
+                switch (hit)
+                {
+                    case 0:
+                        posX += 40;
+                        hit++;
+                        break;
+                    case 1:
+                        posX -= 40;
+                        posY -= 40;
+                        hit = 0;
+                        break;
+                }
+                
+            }
+        }
+    }
+    
+}
